358fa52dbc907bf0cb1df97a2c8d0e153f49a147
Wilmer Miguel Castillo Tejada <wilmer_castillo@ucne.edu.do>
2025-05-16 16:02:13 -0400

    Creación Screen

---
+++ app/src/main/java/edu/ucne/registrotecnicos/presentation/navigation/Screen.kt
@@ -0,0 +1,10 @@
+package edu.ucne.registrotecnicos.presentation.navigation
+
+import kotlinx.serialization.Serializable
+
+sealed class Screen {
+    @Serializable
+    data object TecnicoList : Screen()
+    @Serializable
+    data class Tecnico(val tecnicoId: Int?) : Screen()
+}
