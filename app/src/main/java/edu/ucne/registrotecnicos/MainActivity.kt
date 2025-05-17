de63a189865fa54d1e851d6d37052b65a984ced3
Wilmer Miguel Castillo Tejada <wilmer_castillo@ucne.edu.do>
2025-05-16 20:28:02 -0400

    Modificación del MainActivity

--- app/src/main/java/edu/ucne/registrotecnicos/MainActivity.kt
+++ app/src/main/java/edu/ucne/registrotecnicos/MainActivity.kt
@@ -4,67 +4,67 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.BorderStroke
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.lazy.items
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Add
-import androidx.compose.material.icons.filled.Delete
-import androidx.compose.material.icons.filled.Edit
-import androidx.compose.material3.ButtonDefaults
-import androidx.compose.material3.CardDefaults
-import androidx.compose.material3.ElevatedCard
-import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.HorizontalDivider
-import androidx.compose.material3.Icon
-import androidx.compose.material3.IconButton
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.OutlinedButton
-import androidx.compose.material3.OutlinedTextField
-import androidx.compose.material3.OutlinedTextFieldDefaults
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.material3.TopAppBar
-import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.rememberCoroutineScope
-import androidx.compose.runtime.setValue
-import androidx.compose.ui.Alignment
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
 import androidx.lifecycle.Lifecycle
 import androidx.lifecycle.compose.LocalLifecycleOwner
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import androidx.navigation.compose.rememberNavController
 import androidx.room.Room
-import edu.ucne.registrotecnicos.data.local.dao.TecnicoDao
 import edu.ucne.registrotecnicos.data.local.database.TecnicoDb
-import edu.ucne.registrotecnicos.data.local.entities.TecnicoEntity
-import edu.ucne.registrotecnicos.data.repository.TecnicosRepository
-import edu.ucne.registrotecnicos.presentation.navigation.TecnicosNavHost
-import edu.ucne.registrotecnicos.presentation.tecnicos.TecnicoListScreen
-import edu.ucne.registrotecnicos.presentation.tecnicos.TecnicoScreen
-import edu.ucne.registrotecnicos.presentation.tecnicos.TecnicosViewModel
+import edu.ucne.registrotecnicos.data.repository.PrioridadesRepository
+import edu.ucne.registrotecnicos.presentation.navigation.PrioridadesNavHost
+import edu.ucne.registrotecnicos.presentation.prioridades.PrioridadesViewModel
 import edu.ucne.registrotecnicos.ui.theme.RegistroTecnicosTheme
-import kotlinx.coroutines.MainScope
-import kotlinx.coroutines.launch
+
+//class MainActivity : ComponentActivity() {
+//    private lateinit var tecnicoDb: TecnicoDb
+//    private lateinit var tecnicosRepository: TecnicosRepository
+//    private lateinit var tecnicosViewModel: TecnicosViewModel
+//
+//    override fun onCreate(savedInstanceState: Bundle?) {
+//        super.onCreate(savedInstanceState)
+//        enableEdgeToEdge()
+//
+//        tecnicoDb = Room.databaseBuilder(
+//            applicationContext,
+//            TecnicoDb::class.java,
+//            "Tecnico.db"
+//        ).fallbackToDestructiveMigration()
+//            .build()
+//
+//        tecnicosRepository = TecnicosRepository(tecnicoDb.TecnicoDao())
+//
+//        tecnicosViewModel = TecnicosViewModel(tecnicosRepository)
+//
+//        setContent {
+//            val lifecycleOwner = LocalLifecycleOwner.current
+//            val tecnicoList by tecnicoDb.TecnicoDao().getAll()
+//                .collectAsStateWithLifecycle(
+//                    initialValue = emptyList(),
+//                    lifecycleOwner = lifecycleOwner,
+//                    minActiveState = Lifecycle.State.STARTED
+//                )
+//
+//            RegistroTecnicosTheme {
+//                val nav = rememberNavController()
+//                TecnicosNavHost(
+//                    nav,
+//                    tecnicoList,
+//                    tecnicosViewModel,
+//                    nav
+//                )
+//            }
+//        }
+//    }
+//
+//
+//
+//}
 
 class MainActivity : ComponentActivity() {
     private lateinit var tecnicoDb: TecnicoDb
-    private lateinit var tecnicosRepository: TecnicosRepository
-    private lateinit var tecnicosViewModel: TecnicosViewModel
+    private lateinit var prioridadesRepository: PrioridadesRepository
+    private lateinit var prioridadesViewModel: PrioridadesViewModel
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -77,13 +77,13 @@ class MainActivity : ComponentActivity() {
         ).fallbackToDestructiveMigration()
             .build()
 
-        tecnicosRepository = TecnicosRepository(tecnicoDb.TecnicoDao())
+        prioridadesRepository = PrioridadesRepository(tecnicoDb.PrioridadDao())
 
-        tecnicosViewModel = TecnicosViewModel(tecnicosRepository)
+        prioridadesViewModel = PrioridadesViewModel(prioridadesRepository)
 
         setContent {
             val lifecycleOwner = LocalLifecycleOwner.current
-            val tecnicoList by tecnicoDb.TecnicoDao().getAll()
+            val prioridadList by tecnicoDb.PrioridadDao().getAll()
                 .collectAsStateWithLifecycle(
                     initialValue = emptyList(),
                     lifecycleOwner = lifecycleOwner,
@@ -92,15 +92,16 @@ class MainActivity : ComponentActivity() {
 
             RegistroTecnicosTheme {
                 val nav = rememberNavController()
-                TecnicosNavHost(
+                PrioridadesNavHost(
                     nav,
-                    tecnicoList,
-                    tecnicosViewModel,
+                    prioridadList,
+                    prioridadesViewModel,
                     nav
                 )
             }
         }
     }
+
 
 
 }
