74eb2f65384cf3e27c49081ac5baa13d4584d9db
Wilmer Miguel Castillo Tejada <wilmer_castillo@ucne.edu.do>
2025-05-17 17:26:51 -0400

    Modificación Screen

--- app/src/main/java/edu/ucne/registrotecnicos/presentation/navigation/Screen.kt
+++ app/src/main/java/edu/ucne/registrotecnicos/presentation/navigation/Screen.kt
@@ -6,10 +6,22 @@ import kotlinx.serialization.Serializable
 sealed class Screen {
     @Serializable
     data object TecnicoList : Screen()
+
     @Serializable
     data class Tecnico(val tecnicoId: Int?) : Screen()
+
     @Serializable
     data object PrioridadList : Screen()
+
     @Serializable
     data class Prioridad(val prioridadId: Int?) : Screen()
+
+    @Serializable
+    data object TicketList : Screen()
+
+    @Serializable
+    data class Ticket(val ticketId: Int?) : Screen()
+
+    @Serializable
+    data object Home : Screen()
 }
