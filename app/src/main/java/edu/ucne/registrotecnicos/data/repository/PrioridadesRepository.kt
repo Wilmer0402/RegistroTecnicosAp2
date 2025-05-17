56555b46fd73b6b8ec69dc5c4f40dd061b349f8f
Wilmer Miguel Castillo Tejada <wilmer_castillo@ucne.edu.do>
2025-05-16 20:23:49 -0400

    Creación PrioridadesRepository

---
+++ app/src/main/java/edu/ucne/registrotecnicos/data/repository/PrioridadesRepository.kt
@@ -0,0 +1,17 @@
+package edu.ucne.registrotecnicos.data.repository
+
+import edu.ucne.registrotecnicos.data.local.dao.PrioridadDao
+import edu.ucne.registrotecnicos.data.local.entities.PrioridadEntity
+import kotlinx.coroutines.flow.Flow
+
+class PrioridadesRepository(
+    private val dao: PrioridadDao
+) {
+    suspend fun save(prioridad: PrioridadEntity) = dao.save(prioridad)
+
+    suspend fun find(id: Int): PrioridadEntity? = dao.find(id)
+
+    suspend fun delete(prioridad: PrioridadEntity) = dao.delete(prioridad)
+
+    fun getAll(): Flow<List<PrioridadEntity?>> = dao.getAll()
+}
