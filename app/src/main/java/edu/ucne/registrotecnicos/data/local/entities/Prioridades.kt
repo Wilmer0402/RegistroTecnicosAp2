4fee201e169ceaa1e98e7d8acb73aa886f8f0e39
Wilmer Miguel Castillo Tejada <wilmer_castillo@ucne.edu.do>
2025-05-16 20:22:42 -0400

    Creación PrioridadesEntity

---
+++ app/src/main/java/edu/ucne/registrotecnicos/data/local/entities/Prioridades.kt
@@ -0,0 +1,11 @@
+package edu.ucne.registrotecnicos.data.local.entities
+
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "Prioridades")
+data class PrioridadEntity(
+    @PrimaryKey
+    val prioridadId: Int? = null,
+    val descripcion: String = ""
+)
