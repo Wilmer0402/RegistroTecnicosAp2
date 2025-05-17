8652e661455418ef848c1e3271c1299d6ac65c52
Wilmer Miguel Castillo Tejada <wilmer_castillo@ucne.edu.do>
2025-05-16 20:27:45 -0400

    Modificación del TecnicoDb

--- app/src/main/java/edu/ucne/registrotecnicos/data/local/database/TecnicoDb.kt
+++ app/src/main/java/edu/ucne/registrotecnicos/data/local/database/TecnicoDb.kt
@@ -2,17 +2,22 @@ package edu.ucne.registrotecnicos.data.local.database
 
 import androidx.room.Database
 import androidx.room.RoomDatabase
+import edu.ucne.registrotecnicos.data.local.dao.PrioridadDao
 import edu.ucne.registrotecnicos.data.local.dao.TecnicoDao
 import edu.ucne.registrotecnicos.data.local.entities.TecnicoEntity
 import junit.runner.Version
-
+import  edu.ucne.registrotecnicos.data.local.entities.PrioridadEntity
 @Database(
     entities = [
-        TecnicoEntity::class
+        TecnicoEntity::class,
+    PrioridadEntity::class
+
     ],
-    version = 1,
+    version = 2,
     exportSchema = false
 )
 abstract class TecnicoDb : RoomDatabase() {
     abstract fun TecnicoDao(): TecnicoDao
+    abstract fun PrioridadDao(): PrioridadDao
+
 }
