7c42ca5133369a8fa00e7334310516770f24dbe0
Wilmer Miguel Castillo Tejada <wilmer_castillo@ucne.edu.do>
2025-05-17 17:26:36 -0400

    Modificación TecnicoDb

--- app/src/main/java/edu/ucne/registrotecnicos/data/local/database/TecnicoDb.kt
+++ app/src/main/java/edu/ucne/registrotecnicos/data/local/database/TecnicoDb.kt
@@ -2,22 +2,40 @@ package edu.ucne.registrotecnicos.data.local.database
 
 import androidx.room.Database
 import androidx.room.RoomDatabase
+import androidx.room.TypeConverter
+import androidx.room.TypeConverters
 import edu.ucne.registrotecnicos.data.local.dao.PrioridadDao
 import edu.ucne.registrotecnicos.data.local.dao.TecnicoDao
+import edu.ucne.registrotecnicos.data.local.dao.TicketDao
 import edu.ucne.registrotecnicos.data.local.entities.TecnicoEntity
-import junit.runner.Version
-import  edu.ucne.registrotecnicos.data.local.entities.PrioridadEntity
+import edu.ucne.registrotecnicos.data.local.entities.PrioridadEntity
+import edu.ucne.registrotecnicos.data.local.entities.TicketEntity
+import java.util.Date
+
+class Converters {
+    @TypeConverter
+    fun fromTimestamp(value: Long?): Date? {
+        return value?.let { Date(it) }
+    }
+
+    @TypeConverter
+    fun dateToTimestamp(date: Date?): Long? {
+        return date?.time
+    }
+}
+
 @Database(
     entities = [
         TecnicoEntity::class,
-    PrioridadEntity::class
-
+        PrioridadEntity::class,
+        TicketEntity::class
     ],
-    version = 2,
+    version = 3,
     exportSchema = false
 )
+@TypeConverters(Converters::class)
 abstract class TecnicoDb : RoomDatabase() {
     abstract fun TecnicoDao(): TecnicoDao
     abstract fun PrioridadDao(): PrioridadDao
-
+    abstract fun TicketDao(): TicketDao
 }
