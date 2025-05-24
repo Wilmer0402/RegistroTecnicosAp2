5ba7b1d65b25e04cc2ffd415714faee24ad85aad
Wilmer Miguel Castillo Tejada <wilmer_castillo@ucne.edu.do>
2025-05-24 13:22:26 -0400

    Modificación AppModule

--- app/src/main/java/edu/ucne/registrotecnicos/di/AppModule.kt
+++ app/src/main/java/edu/ucne/registrotecnicos/di/AppModule.kt
@@ -28,4 +28,6 @@ object AppModule {
     fun providePrioridadDao(tecnicoDb: TecnicoDb) = tecnicoDb.PrioridadDao()
     @Provides
     fun provideTicketDao(tecnicoDb: TecnicoDb) = tecnicoDb.TicketDao()
+    @Provides
+    fun provideMensajeDao(tecnicoDb: TecnicoDb) = tecnicoDb.MensajeDao()
 }
