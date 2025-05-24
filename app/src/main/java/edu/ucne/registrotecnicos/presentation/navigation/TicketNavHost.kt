05b6e791836b705a74f12ee76c0743105e26b453
Wilmer Miguel Castillo Tejada <wilmer_castillo@ucne.edu.do>
2025-05-24 01:46:08 -0400

    Modificación TicketNavHost

--- app/src/main/java/edu/ucne/registrotecnicos/presentation/navigation/TicketNavHost.kt
+++ app/src/main/java/edu/ucne/registrotecnicos/presentation/navigation/TicketNavHost.kt
@@ -1,47 +1,47 @@
-package edu.ucne.registrotecnicos.presentation.navigation
-
-import androidx.compose.runtime.Composable
-import androidx.navigation.NavController
-import androidx.navigation.NavHostController
-import androidx.navigation.compose.NavHost
-import androidx.navigation.compose.composable
-import androidx.navigation.toRoute
-import edu.ucne.registrotecnicos.data.local.entities.TicketEntity
-import edu.ucne.registrotecnicos.presentation.tickets.TicketListScreen
-import edu.ucne.registrotecnicos.presentation.tickets.TicketScreen
-import edu.ucne.registrotecnicos.presentation.tickets.TicketsViewModel
-
-@Composable
-fun TicketsNavHost(
-    navHostController: NavHostController,
-    ticketList: List<TicketEntity>,
-    viewModel: TicketsViewModel,
-    navController: NavController
-) {
-    NavHost(
-        navController = navHostController,
-        startDestination = Screen.TicketList
-    ) {
-        composable<Screen.TicketList> {
-            TicketListScreen(
-                ticketList = ticketList,
-                onEdit = { ticketId ->
-                    navHostController.navigate(Screen.Ticket(ticketId))
-                },
-                onDelete = { ticket ->
-                    viewModel.deleteTicket(ticket)
-                }
-            )
-        }
-
-        composable<Screen.Ticket>{ backStack ->
-            val ticketId  = backStack.toRoute<Screen.Ticket>().ticketId
-            TicketScreen(
-                ticketId,
-                viewModel,
-                navController
-            ) {
-            }
-        }
-    }
-}
+//package edu.ucne.registrotecnicos.presentation.navigation
+//
+//import androidx.compose.runtime.Composable
+//import androidx.navigation.NavController
+//import androidx.navigation.NavHostController
+//import androidx.navigation.compose.NavHost
+//import androidx.navigation.compose.composable
+//import androidx.navigation.toRoute
+//import edu.ucne.registrotecnicos.data.local.entities.TicketEntity
+//import edu.ucne.registrotecnicos.presentation.tickets.TicketListScreen
+//import edu.ucne.registrotecnicos.presentation.tickets.TicketScreen
+//import edu.ucne.registrotecnicos.presentation.tickets.TicketsViewModel
+//
+//@Composable
+//fun TicketsNavHost(
+//    navHostController: NavHostController,
+//    ticketList: List<TicketEntity>,
+//    viewModel: TicketsViewModel,
+//    navController: NavController
+//) {
+//    NavHost(
+//        navController = navHostController,
+//        startDestination = Screen.TicketList
+//    ) {
+//        composable<Screen.TicketList> {
+//            TicketListScreen(
+//                ticketList = ticketList,
+//                onEdit = { ticketId ->
+//                    navHostController.navigate(Screen.Ticket(ticketId))
+//                },
+//                onDelete = { ticket ->
+//                    viewModel.deleteTicket(ticket)
+//                }
+//            )
+//        }
+//
+//        composable<Screen.Ticket>{ backStack ->
+//            val ticketId  = backStack.toRoute<Screen.Ticket>().ticketId
+//            TicketScreen(
+//                ticketId,
+//                viewModel,
+//                navController
+//            ) {
+//            }
+//        }
+//    }
+//}
