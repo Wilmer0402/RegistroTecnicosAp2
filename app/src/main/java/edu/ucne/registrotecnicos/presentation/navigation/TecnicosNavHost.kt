31f66947b1ab84a0c47b1960390ae7b6228d09bd
Wilmer Miguel Castillo Tejada <wilmer_castillo@ucne.edu.do>
2025-05-24 01:45:04 -0400

    Modificación TecnicosNavHost

--- app/src/main/java/edu/ucne/registrotecnicos/presentation/navigation/TecnicosNavHost.kt
+++ app/src/main/java/edu/ucne/registrotecnicos/presentation/navigation/TecnicosNavHost.kt
@@ -1,47 +1,47 @@
-package edu.ucne.registrotecnicos.presentation.navigation
-
-import androidx.compose.runtime.Composable
-import androidx.navigation.NavController
-import androidx.navigation.NavHostController
-import androidx.navigation.compose.NavHost
-import androidx.navigation.compose.composable
-import androidx.navigation.toRoute
-import edu.ucne.registrotecnicos.data.local.entities.TecnicoEntity
-import edu.ucne.registrotecnicos.presentation.tecnicos.TecnicoListScreen
-import edu.ucne.registrotecnicos.presentation.tecnicos.TecnicoScreen
-import edu.ucne.registrotecnicos.presentation.tecnicos.TecnicosViewModel
-
-@Composable
-fun TecnicosNavHost(
-    navHostController: NavHostController,
-    tecnicoList: List<TecnicoEntity>,
-    viewModel: TecnicosViewModel,
-    navController: NavController
-) {
-    NavHost(
-        navController = navHostController,
-        startDestination = Screen.TecnicoList
-    ) {
-        composable<Screen.TecnicoList> {
-            TecnicoListScreen(
-                tecnicoList = tecnicoList,
-                onEdit = { tecnicoId ->
-                    navHostController.navigate(Screen.Tecnico(tecnicoId))
-                },
-                onDelete = { tecnico ->
-                    viewModel.deleteTecnico(tecnico)
-                }
-            )
-        }
-
-        composable<Screen.Tecnico>{ backStack ->
-            val tecnicoId  = backStack.toRoute<Screen.Tecnico>().tecnicoId
-            TecnicoScreen(
-                tecnicoId,
-                viewModel,
-                navController
-            ) {
-            }
-        }
-    }
-}
+//package edu.ucne.registrotecnicos.presentation.navigation
+//
+//import androidx.compose.runtime.Composable
+//import androidx.navigation.NavController
+//import androidx.navigation.NavHostController
+//import androidx.navigation.compose.NavHost
+//import androidx.navigation.compose.composable
+//import androidx.navigation.toRoute
+//import edu.ucne.registrotecnicos.data.local.entities.TecnicoEntity
+//import edu.ucne.registrotecnicos.presentation.tecnicos.TecnicoListScreen
+//import edu.ucne.registrotecnicos.presentation.tecnicos.TecnicoScreen
+//import edu.ucne.registrotecnicos.presentation.tecnicos.TecnicosViewModel
+//
+//@Composable
+//fun TecnicosNavHost(
+//    navHostController: NavHostController,
+//    tecnicoList: List<TecnicoEntity>,
+//    viewModel: TecnicosViewModel,
+//    navControl: NavController
+//) {
+//    NavHost(
+//        navController = navHostController,
+//        startDestination = Screen.TecnicoList
+//    ) {
+//        composable<Screen.TecnicoList> {
+//            TecnicoListScreen(
+//                tecnicoList = tecnicoList,
+//                onEdit = { tecnicoId ->
+//                    navHostController.navigate(Screen.Tecnico(tecnicoId))
+//                },
+//                onDelete = { tecnico ->
+//                    viewModel.deleteTecnico(tecnico)
+//                }
+//            )
+//        }
+//
+//        composable<Screen.Tecnico>{ backStack ->
+//            val tecnicoId  = backStack.toRoute<Screen.Tecnico>().tecnicoId
+//            TecnicoScreen(
+//                tecnicoId,
+//                viewModel,
+//                navControl
+//            ) {
+//            }
+//        }
+//    }
+//}
