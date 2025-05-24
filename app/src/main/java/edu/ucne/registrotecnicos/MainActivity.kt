2487d770b94f0e47d7eaf5e7200b1886d5764cdb
Wilmer Miguel Castillo Tejada <wilmer_castillo@ucne.edu.do>
2025-05-24 01:42:01 -0400

    Modificación MainActivity

--- app/src/main/java/edu/ucne/registrotecnicos/MainActivity.kt
+++ app/src/main/java/edu/ucne/registrotecnicos/MainActivity.kt
@@ -4,92 +4,93 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.Scaffold
 import androidx.compose.runtime.getValue
+import androidx.compose.ui.Modifier
 import androidx.lifecycle.Lifecycle
 import androidx.lifecycle.compose.LocalLifecycleOwner
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import androidx.navigation.compose.rememberNavController
 import androidx.room.Room
+import dagger.hilt.android.AndroidEntryPoint
 import edu.ucne.registrotecnicos.data.local.database.TecnicoDb
 import edu.ucne.registrotecnicos.data.repository.PrioridadesRepository
 import edu.ucne.registrotecnicos.data.repository.TecnicosRepository
 import edu.ucne.registrotecnicos.data.repository.TicketsRepository
 import edu.ucne.registrotecnicos.presentation.navigation.HomeNavHost
-import edu.ucne.registrotecnicos.presentation.navigation.PrioridadesNavHost
-import edu.ucne.registrotecnicos.presentation.navigation.TicketsNavHost
+//import edu.ucne.registrotecnicos.presentation.navigation.PrioridadesNavHost
+//import edu.ucne.registrotecnicos.presentation.navigation.TicketsNavHost
 import edu.ucne.registrotecnicos.presentation.prioridades.PrioridadesViewModel
 import edu.ucne.registrotecnicos.presentation.tecnicos.TecnicosViewModel
 import edu.ucne.registrotecnicos.presentation.tickets.TicketsViewModel
 import edu.ucne.registrotecnicos.ui.theme.RegistroTecnicosTheme
-import edu.ucne.registrotecnicos.presentation.navigation.TecnicosNavHost
+//import edu.ucne.registrotecnicos.presentation.navigation.TecnicosNavHost
 
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     private lateinit var tecnicoDb: TecnicoDb
     private lateinit var tecnicosRepository: TecnicosRepository
     private lateinit var tecnicosViewModel: TecnicosViewModel
+
     private lateinit var prioridadesRepository: PrioridadesRepository
-    private lateinit var prioridadesViewModel: PrioridadesViewModel
+    private lateinit var prioridadViewModel: PrioridadesViewModel
+
     private lateinit var ticketsRepository: TicketsRepository
     private lateinit var ticketsViewModel: TicketsViewModel
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
+
         tecnicoDb = Room.databaseBuilder(
             applicationContext,
             TecnicoDb::class.java,
             "Tecnico.db"
-        ).fallbackToDestructiveMigration().build()
+        ).fallbackToDestructiveMigration()
+            .build()
 
         tecnicosRepository = TecnicosRepository(tecnicoDb.TecnicoDao())
         tecnicosViewModel = TecnicosViewModel(tecnicosRepository)
 
         prioridadesRepository = PrioridadesRepository(tecnicoDb.PrioridadDao())
-        prioridadesViewModel = PrioridadesViewModel(prioridadesRepository)
+        prioridadViewModel = PrioridadesViewModel(prioridadesRepository)
 
         ticketsRepository = TicketsRepository(tecnicoDb.TicketDao())
         ticketsViewModel = TicketsViewModel(
-            ticketsRepository = ticketsRepository,
-            tecnicosRepository = tecnicosRepository,
-            prioridadesRepository = prioridadesRepository
+            ticketsRepository,
+            tecnicosRepository,
+            prioridadesRepository
         )
 
         setContent {
-            RegistroTecnicosTheme {
-                val lifecycleOwner = LocalLifecycleOwner.current
+            val lifecycleOwner = LocalLifecycleOwner.current
 
-                val tecnicoList by tecnicoDb.TecnicoDao().getAll()
-                    .collectAsStateWithLifecycle(
-                        initialValue = emptyList(),
-                        lifecycleOwner = lifecycleOwner,
-                        minActiveState = Lifecycle.State.STARTED
-                    )
-
-                val ticketList by tecnicoDb.TicketDao().getAll()
-                    .collectAsStateWithLifecycle(
-                        initialValue = emptyList(),
-                        lifecycleOwner = lifecycleOwner,
-                        minActiveState = Lifecycle.State.STARTED
-                    )
-
-                val prioridadList by tecnicoDb.PrioridadDao().getAll()
-                    .collectAsStateWithLifecycle(
-                        initialValue = emptyList(),
-                        lifecycleOwner = lifecycleOwner,
-                        minActiveState = Lifecycle.State.STARTED
-                    )
-
-                val navController = rememberNavController()
-
-                HomeNavHost(
-                    navController = navController,
-                    tecnicoList = tecnicoList,
-                    ticketList = ticketList,
-                    prioridadList = prioridadList,
-                    tecnicosViewModel = tecnicosViewModel,
-                    ticketsViewModel = ticketsViewModel,
-                    prioridadesViewModel = prioridadesViewModel
+            val ticketList by tecnicoDb.TicketDao().getAll()
+                .collectAsStateWithLifecycle(
+                    initialValue = emptyList(),
+                    lifecycleOwner = lifecycleOwner,
+                    minActiveState = Lifecycle.State.STARTED
                 )
+
+            RegistroTecnicosTheme {
+                val nav = rememberNavController()
+
+                Scaffold(
+                    modifier = Modifier.
+                    fillMaxSize()
+                ) { paddingValues ->
+                    Box(modifier = Modifier.padding(paddingValues)) {
+                        HomeNavHost(
+                            navHostController = nav,
+                            prioridadesViewModel = prioridadViewModel,
+                            tecnicosViewModel = tecnicosViewModel,
+                            ticketsViewModel = ticketsViewModel
+                        )
+                    }
+                }
             }
         }
     }
