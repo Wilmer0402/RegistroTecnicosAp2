594fe271ae555b3cf38e988451d5bc1b484125be
Wilmer Miguel Castillo Tejada <wilmer_castillo@ucne.edu.do>
2025-05-24 01:42:21 -0400

    Modificación PrioridadesNavHost

--- app/src/main/java/edu/ucne/registrotecnicos/presentation/navigation/PrioridadesNavHost.kt
+++ app/src/main/java/edu/ucne/registrotecnicos/presentation/navigation/PrioridadesNavHost.kt
@@ -1,47 +1,47 @@
-package edu.ucne.registrotecnicos.presentation.navigation
-
-import androidx.compose.runtime.Composable
-import androidx.navigation.NavController
-import androidx.navigation.NavHostController
-import androidx.navigation.compose.NavHost
-import androidx.navigation.compose.composable
-import androidx.navigation.toRoute
-import edu.ucne.registrotecnicos.data.local.entities.PrioridadEntity
-import edu.ucne.registrotecnicos.presentation.prioridades.PrioridadListScreen
-import edu.ucne.registrotecnicos.presentation.prioridades.PrioridadScreen
-import edu.ucne.registrotecnicos.presentation.prioridades.PrioridadesViewModel
-
-@Composable
-fun PrioridadesNavHost(
-    navHostController: NavHostController,
-    prioridadList: List<PrioridadEntity>,
-    viewModel: PrioridadesViewModel,
-    navcontrol: NavController
-) {
-    NavHost(
-        navController = navHostController,
-        startDestination = Screen.PrioridadList
-    ) {
-        composable<Screen.PrioridadList> {
-            PrioridadListScreen(
-                prioridadList = prioridadList,
-                onEdit = { prioridadId ->
-                    navHostController.navigate(Screen.Prioridad(prioridadId))
-                },
-                onDelete = { prioridad ->
-                    viewModel.deletePrioridad(prioridad)
-                }
-            )
-        }
-
-        composable<Screen.Prioridad> { backStack ->
-            val prioridadId = backStack.toRoute<Screen.Prioridad>().prioridadId
-            PrioridadScreen(
-                prioridadId,
-                viewModel,
-                navcontrol
-            )
-
-        }
-    }
-}
+//package edu.ucne.registrotecnicos.presentation.navigation
+//
+//import androidx.compose.runtime.Composable
+//import androidx.navigation.NavController
+//import androidx.navigation.NavHostController
+//import androidx.navigation.compose.NavHost
+//import androidx.navigation.compose.composable
+//import androidx.navigation.toRoute
+//import edu.ucne.registrotecnicos.data.local.entities.PrioridadEntity
+//import edu.ucne.registrotecnicos.presentation.prioridades.PrioridadListScreen
+//import edu.ucne.registrotecnicos.presentation.prioridades.PrioridadScreen
+//import edu.ucne.registrotecnicos.presentation.prioridades.PrioridadesViewModel
+//
+//@Composable
+//fun PrioridadesNavHost(
+//    navHostController: NavHostController,
+//    prioridadList: List<PrioridadEntity>,
+//    viewModel: PrioridadesViewModel,
+//    navController: NavController
+//) {
+//    NavHost(
+//        navController = navHostController,
+//        startDestination = Screen.PrioridadList
+//    ) {
+//        composable<Screen.PrioridadList> {
+//            PrioridadListScreen(
+//                prioridadList = prioridadList,
+//                onEdit = { prioridadId ->
+//                    navHostController.navigate(Screen.Prioridad(prioridadId))
+//                },
+//                onDelete = { prioridad ->
+//                    viewModel.deletePrioridad(prioridad)
+//                }
+//            )
+//        }
+//
+//        composable<Screen.Prioridad> { backStack ->
+//            val prioridadId = backStack.toRoute<Screen.Prioridad>().prioridadId
+//            PrioridadScreen(
+//                prioridadId,
+//                viewModel,
+//                navController
+//            ) {
+//            }
+//        }
+//    }
+//}
