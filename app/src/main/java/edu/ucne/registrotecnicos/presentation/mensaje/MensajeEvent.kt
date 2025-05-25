50e94160ff704d9b780a5fc2b305b62b651f4f7d
Wilmer Miguel Castillo Tejada <wilmer_castillo@ucne.edu.do>
2025-05-24 21:53:16 -0400

    Modificación MensajeEvent

--- app/src/main/java/edu/ucne/registrotecnicos/presentation/mensaje/MensajeEvent.kt
+++ app/src/main/java/edu/ucne/registrotecnicos/presentation/mensaje/MensajeEvent.kt
@@ -9,6 +9,7 @@ sealed interface MensajeEvent {
     data class ContenidoChange(val contenido: String): MensajeEvent
     data class RemitenteChange(val remitente: String): MensajeEvent
     data class TicketIdChange(val ticketId: Int): MensajeEvent
+    data class TipoRemitenteChange(val tipoRemitente: String): MensajeEvent
     data object Save: MensajeEvent
     data object Delete: MensajeEvent
     data object New: MensajeEvent
