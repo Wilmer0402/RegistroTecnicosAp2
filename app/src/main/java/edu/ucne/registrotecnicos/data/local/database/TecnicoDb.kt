7925ed9a15d22287841c66a932584f93716bee96
Wilmer Miguel Castillo Tejada <wilmer_castillo@ucne.edu.do>
2025-05-24 21:57:02 -0400

    Modificación TecnicoDb

--- app/src/main/java/edu/ucne/registrotecnicos/data/local/database/TecnicoDb.kt
+++ app/src/main/java/edu/ucne/registrotecnicos/data/local/database/TecnicoDb.kt
@@ -33,7 +33,7 @@ class Converters {
         TicketEntity::class,
         MensajeEntity::class
     ],
-    version = 3,
+    version = 4,
     exportSchema = false
 )
 @TypeConverters(Converters::class)
