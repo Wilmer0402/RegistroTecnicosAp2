a3e1435898d883ecac8e663d5b078997979d7843
Wilmer Miguel Castillo Tejada <wilmer_castillo@ucne.edu.do>
2025-05-24 21:52:50 -0400

    Modificación MainActivity

--- app/src/main/java/edu/ucne/registrotecnicos/MainActivity.kt
+++ app/src/main/java/edu/ucne/registrotecnicos/MainActivity.kt
@@ -8,38 +8,17 @@ import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material.Scaffold
-import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
-import androidx.lifecycle.Lifecycle
-import androidx.lifecycle.compose.LocalLifecycleOwner
-import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import androidx.navigation.compose.rememberNavController
 import androidx.room.Room
 import dagger.hilt.android.AndroidEntryPoint
 import edu.ucne.registrotecnicos.data.local.database.TecnicoDb
-import edu.ucne.registrotecnicos.data.repository.PrioridadesRepository
-import edu.ucne.registrotecnicos.data.repository.TecnicosRepository
-import edu.ucne.registrotecnicos.data.repository.TicketsRepository
 import edu.ucne.registrotecnicos.presentation.navigation.HomeNavHost
-//import edu.ucne.registrotecnicos.presentation.navigation.PrioridadesNavHost
-//import edu.ucne.registrotecnicos.presentation.navigation.TicketsNavHost
-import edu.ucne.registrotecnicos.presentation.prioridades.PrioridadesViewModel
-import edu.ucne.registrotecnicos.presentation.tecnicos.TecnicosViewModel
-import edu.ucne.registrotecnicos.presentation.tickets.TicketsViewModel
 import edu.ucne.registrotecnicos.ui.theme.RegistroTecnicosTheme
-//import edu.ucne.registrotecnicos.presentation.navigation.TecnicosNavHost
 
 @AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     private lateinit var tecnicoDb: TecnicoDb
-    private lateinit var tecnicosRepository: TecnicosRepository
-    private lateinit var tecnicosViewModel: TecnicosViewModel
-
-    private lateinit var prioridadesRepository: PrioridadesRepository
-    private lateinit var prioridadViewModel: PrioridadesViewModel
-
-    private lateinit var ticketsRepository: TicketsRepository
-    private lateinit var ticketsViewModel: TicketsViewModel
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -52,42 +31,16 @@ class MainActivity : ComponentActivity() {
         ).fallbackToDestructiveMigration()
             .build()
 
-        tecnicosRepository = TecnicosRepository(tecnicoDb.TecnicoDao())
-        tecnicosViewModel = TecnicosViewModel(tecnicosRepository)
-
-        prioridadesRepository = PrioridadesRepository(tecnicoDb.PrioridadDao())
-        prioridadViewModel = PrioridadesViewModel(prioridadesRepository)
-
-        ticketsRepository = TicketsRepository(tecnicoDb.TicketDao())
-        ticketsViewModel = TicketsViewModel(
-            ticketsRepository,
-            tecnicosRepository,
-            prioridadesRepository
-        )
-
         setContent {
-            val lifecycleOwner = LocalLifecycleOwner.current
-
-            val ticketList by tecnicoDb.TicketDao().getAll()
-                .collectAsStateWithLifecycle(
-                    initialValue = emptyList(),
-                    lifecycleOwner = lifecycleOwner,
-                    minActiveState = Lifecycle.State.STARTED
-                )
-
             RegistroTecnicosTheme {
                 val nav = rememberNavController()
 
                 Scaffold(
-                    modifier = Modifier.
-                    fillMaxSize()
+                    modifier = Modifier.fillMaxSize()
                 ) { paddingValues ->
                     Box(modifier = Modifier.padding(paddingValues)) {
                         HomeNavHost(
-                            navHostController = nav,
-                            prioridadesViewModel = prioridadViewModel,
-                            tecnicosViewModel = tecnicosViewModel,
-                            ticketsViewModel = ticketsViewModel
+                            navHostController = nav
                         )
                     }
                 }
