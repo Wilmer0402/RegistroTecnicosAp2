803b82f7dcf18ac893a243397b482dadcb19c6fa
Wilmer Miguel Castillo Tejada <wilmer_castillo@ucne.edu.do>
2025-06-16 14:46:02 -0400

    Creación TecnicEvent

---
+++ app/src/main/java/edu/ucne/registrotecnicos/presentation/tecnico/TecnicEvent.kt
@@ -0,0 +1,15 @@
+package edu.ucne.registrotecnicos.presentation.tecnico
+
+sealed interface TecnicEvent {
+    data class  TecnicIdChange(val tecnicoId: Int): TecnicEvent
+    data class NombreChange(val nombre: String): TecnicEvent
+    data class SueldoChange(val sueldo: Double): TecnicEvent
+
+    data object PostTecnic: TecnicEvent
+    data object GetTecnic: TecnicEvent
+    data object Nuevo: TecnicEvent
+    data object LimpiarErrorNombre: TecnicEvent
+    data object LimpiarErrorSueldo: TecnicEvent
+    data class GetTecnicos(val id : Int): TecnicEvent
+    data object ResetSuccessMessage: TecnicEvent
+}
