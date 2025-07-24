ebcbcbac8db9ff64c7aff2db754aabace42e1d3d
Wilmer Miguel Castillo Tejada <wilmer_castillo@ucne.edu.do>
2025-07-23 23:26:56 -0400

    Modificacion en RemoteDataSource

--- app/src/main/java/edu/ucne/registrotecnicos/data/remote/RemoteDataSource.kt
+++ app/src/main/java/edu/ucne/registrotecnicos/data/remote/RemoteDataSource.kt
@@ -1,5 +1,6 @@
 package edu.ucne.registrotecnicos.data.remote
 
+import edu.ucne.registrotecnicos.data.remote.dto.SistemasDto
 import edu.ucne.registrotecnicos.data.remote.dto.TecnicDto
 import javax.inject.Inject
 
@@ -17,4 +18,18 @@ class RemoteDataSource @Inject constructor(
     suspend fun deleteTecnic(id: Int) = tecnicoingApi.deleteTecnic(id)
 
     suspend fun getTecnic(id: Int) = tecnicoingApi.getTecnic(id)
+
+    //Sistemas
+
+    suspend fun getSistemas() = tecnicoingApi.getSistemas()
+
+    suspend fun  updateSistemas(sistemasDto: SistemasDto) = tecnicoingApi.updateSistemas(sistemasDto)
+
+    suspend fun  saveSistemas(sistemasDto: SistemasDto) = tecnicoingApi.saveSistemas(sistemasDto)
+
+    suspend fun deleteSistemas(id: Int) = tecnicoingApi.deleteSistemas(id)
+
+    suspend fun getSistemas(id:Int) = tecnicoingApi.getSistemas(id)
+
+
 }
